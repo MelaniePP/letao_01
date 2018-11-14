9f47e97e5aeb788e1ffb15ede8857634ed44b9e5
mayuping <myp@qq.com>
2018-11-14 11:40:24 +0800

    登录功能完成

---
+++ js/login.js
@@ -0,0 +1,73 @@
+$(function(){
+  // 1、表单校验功能
+  $("#form").bootstrapValidator({
+    // 配置校验图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',    // 校验成功
+      invalid: 'glyphicon glyphicon-remove',  // 校验失败
+      validating: 'glyphicon glyphicon-refresh'  // 校验中
+    },
+    fields:{
+      username:{
+        validators:{
+          notEmpty:{
+            message:"用户名不能为空"
+          },
+          stringLength:{
+            min:2,
+            max:6,
+            message:"用户名长度为2-6位"
+          },
+          callback:{
+            message:"用户名不存在"
+          }
+        }
+      },
+      password:{
+        validators:{
+          notEmpty:{
+            message:"密码不能为空"
+          },
+          stringLength:{
+            min:6,
+            max:12,
+            message:"密码长度为6-12位"
+          },
+          callback:{
+            message:"密码错误"
+          }
+        }
+      }
+    }
+  })
+
+  // 2.发送请求
+  $("#form").on("success.form.bv",function(e){
+    e.preventDefault();
+    $.ajax({
+      type:"post",
+      url:"/employee/employeeLogin",
+      data:$("#form").serialize(),
+      dataType:"json",
+      success:function(info){
+        if(info.success){
+          location.href = "index.html";
+
+        }
+        if(info.error === 1000){
+          $("#form").data("bootstrapValidator").updateStatus("username","INVALID","callback");
+          // $('#form').data("bootstrapValidator").updateStatus("username","INVALID", "callback");
+
+        }
+        if(info.error === 1001){
+          $("#form").data("bootstrapValidator").updateStatus("password","INVALID","callback");
+        }
+      }
+    })
+  })
+
+  // 3.重置表单
+  $("[type = 'reset']").on("click",function(){
+    $("#form").data("bootstrapValidator").resetForm();
+  })
+})
