e0992966777fde51f47f4b7cb03bce3ceed90196
mayuping <myp@qq.com>
2018-11-16 20:25:11 +0800

    分页完成

--- js/user.js
+++ js/user.js
@@ -1,15 +1,36 @@
 $(function(){
-  $.ajax({
-    type:"get",
-    url:"/user/queryUser",
-    data:{
-      page:1,
-      pageSize:5
-    },
-    dataType:"json",
-    success:function(info){
-      // console.log(info);
-      $("tbody").html(template("tmp",info));
-    }
-  })
+
+  var currentPage = 1;
+  var size = 5;
+  render();
+  function render(){
+
+    $.ajax({
+      type:"get",
+      url:"/user/queryUser",
+      data:{
+        page:currentPage,
+        pageSize:size
+      },
+      dataType:"json",
+      success:function(info){
+        console.log(info);
+        $("tbody").html(template("tmp",info));
+        // 分页标签初始化
+        
+        $("#paginator").bootstrapPaginator({
+          bootstrapMajorVersion:3,
+          page:1,
+          pageSize:5,
+          totalPages:Math.ceil(info.total/info.size),
+          onPageClicked:function(a,b,c,page){
+            currentPage = page;
+            render();
+          }
+        })
+      }
+    })
+  }
+
+
 })
